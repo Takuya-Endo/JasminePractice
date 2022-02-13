d2e4a33668321a78eda4df3d0b7e9f0a5f220866
Takuya-Endo <above5purple@gmail.com>
2022-02-14 07:12:31 +0900

    [Update]JasminePractice-6

--- JavaScriptSample/practiceMDN.js
+++ JavaScriptSample/practiceMDN.js
@@ -1,3 +1,5 @@
+
+/*
 const para = document.querySelector('p');
 
 para.addEventListener('click', updateName);
@@ -6,3 +8,42 @@ function updateName() {
   const name = prompt('Enter a new name');
   para.textContent = `Player 1: ${name}`;
 }
+
+const buttonA = document.getElementById('A');
+const buttonB = document.getElementById('B');
+const buttonC = document.getElementById('C');
+
+//関数を定義して使用する
+function buttonAclick() {
+  alert('buttonA');
+} //html側で呼び出し→<button id="A" onclick="buttonAclick()">
+
+//匿名関数として変数に格納
+const buttonBclick = function() {
+  alert('buttonB');
+}
+
+buttonC.onclick = function() {
+  alert('buttonC');
+}
+*/
+
+
+//-----Object-----
+const UserA = {
+  name: "UserA",
+  age: 20,
+  getName: function() {
+    console.log(this.name);
+    return this.name;
+  },
+  getAge: function() {
+    console.log(this.age);
+    return this.age;
+  }
+}
+//メソッド定義時は()不要、実行時は()必要
+console.log(UserA.name);
+console.log(UserA.age);
+UserA.getName();
+UserA.getAge();
